927a796f7fd696239fc4e812c1dc1d3e010350e3
Balaji Sivaraman <balaji@balajisivaraman.com>
2021-12-14 20:11:32 +0530

    refac: introduce advent_run macro to make it easier to run soln

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(iter_advance_by)]
+#![feature(concat_idents)]
 
 use anyhow::Result;
+use paste::paste;
 use std::env;
 
 mod utils;
@@ -12,26 +14,29 @@ fn main() -> Result<()> {
     let day = args.get(2).unwrap();
     match (year.as_str(), day.as_str()) {
         ("2021", "day01") => {
-            println!(
-                "Year 2021, Day 01, Part 1: {}",
-                year2021::day01a(utils::read_lines_as_i32_vector("input/day01.txt")?)
-            );
-            println!(
-                "Year 2021, Day 01, Part 2: {}",
-                year2021::day01b(utils::read_lines_as_i32_vector("input/day01.txt")?)
-            );
+            advent_run!(2021, 1, utils::read_lines_as_i32_vector);
         }
         ("2021", "day02") => {
-            println!(
-                "Year 2021, Day 02, Part 1: {}",
-                year2021::day02a(utils::read_lines_as_str_vector("input/day02.txt")?)
-            );
-            println!(
-                "Year 2021, Day 02, Part 2: {}",
-                year2021::day02b(utils::read_lines_as_str_vector("input/day02.txt")?)
-            );
+            advent_run!(2021, 2, utils::read_lines_as_str_vector);
         }
         (_, _) => println!("Unknown input"),
     }
+
     Ok(())
+}
+
+#[macro_export]
+macro_rules! advent_run {
+    ($year:expr, $day:expr, $utils:path) => {
+        paste! {
+            let part1 = crate::[<year $year>]::[<day0 $day a>]($utils(format!("input/day0{}.txt", $day).as_str())?);
+            let part2 = crate::[<year $year>]::[<day0 $day b>]($utils(format!("input/day0{}.txt", $day).as_str())?);
+            println!(
+                "Year {}, Day 0{}, Part 1: {}", $year, $day, part1
+            );
+            println!(
+                "Year {}, Day 0{}, Part 2: {}", $year, $day, part2
+            );
+        }
+    };
 }
